cb4b7ac365461bd8bc91cae69ddae6b5b0795e61
Gustav <sir.gustav.the.coder@gmail.com>
2025-05-27 10:57:21 +0200

    Expand with parsing of result

--- Program.cs
+++ Program.cs
@@ -31,20 +31,42 @@ Console.WriteLine($"Using: {settings.SelectedModel}");
 string html = "<html><body>This page will discuss the importance of petting dogs in video games and how it might improve your life. <a href=\"\">Click here</a> to start your dog petting journey.</body></html>";
 var json = JsonSerializer.Serialize(new TagsResponse()
     {
-        Tags = ["list", "of", "tags"]
+        Tags = ["list", "of", "tags"],
+        Reasoning = "the tags were selected as a example"
     }
 );
 var question = $"Given the following html code, extract the tags that would be entered on the social bookmarking site del.icio.us. " +
                $"Feel free to improve the tags. Output a syntactically similar to the following json. " +
-               $"Include the reasoning for choosing he specific tags in the reason property. " +
+               $"Include the reasoning with at least 2 sentences for choosing he specific tags in the reason property. " +
                $"Don't output example code and don't format it as markdown. " +
                $"Only the actual json:\n\n{json}\n\n" +
                $"Html starts now:\n\n{html}";
 
 var res = await Cli.Status("Thinking...", async () => await lm.GetCompletions(settings.SelectedModel, question));
-foreach(var m in res.Choices)
+foreach (var m in res.Choices)
 {
+    var r = JsonSerializer.Deserialize<TagsResponse>(m.Message.Content);
+    try
+    {
+        if (r != null)
+        {
+            var tags = string.Join(",", r.Tags.Select(x => JsonSerializer.Serialize(x)));
+            Console.WriteLine($"{r.Reasoning}");
+            Console.WriteLine($"{tags}");
+            Console.WriteLine();
+            continue;
+        }
+        else
+        {
+            Console.WriteLine($"Parsed a null");
+        }
+    }
+    catch (Exception)
+    {
+        Console.WriteLine($"Not a json...");
+    }
     Console.WriteLine($"> {m.Message.Content}");
+
 }
 
 
