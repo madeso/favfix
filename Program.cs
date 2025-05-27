bbf89bf136a045d9e3e47e7aca43c4d65e901bd5
Gustav <sir.gustav.the.coder@gmail.com>
2025-05-27 10:10:33 +0200

    wip test of tagging

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Collections.Immutable;
 using favfix;
 using Spectre.Console;
+using System.Text.Json.Serialization;
+using System.Text.Json;
 
 var settingsFile = Path.Join(Environment.CurrentDirectory, "settings.json");
 
@@ -25,8 +28,28 @@ if (string.IsNullOrWhiteSpace(settings.SelectedModel))
 
 Console.WriteLine($"Using: {settings.SelectedModel}");
 
-var res = await Cli.Status("Thinking...", async () => await lm.GetCompletions(settings.SelectedModel, "Say this is a test!"));
+string html = "<html><body>This page will discuss the importance of petting dogs in video games and how it might improve your life. <a href=\"\">Click here</a> to start your dog petting journey.</body></html>";
+var json = JsonSerializer.Serialize(new TagsResponse()
+    {
+        Tags = ["list", "of", "tags"]
+    }
+);
+var question = $"Given the following html code, extract the tags that would be entered on the social bookmarking site del.icio.us. " +
+               $"Feel free to improve the tags. Output a syntactically similar to the following json. " +
+               $"Include the reasoning for choosing he specific tags in the reason property. " +
+               $"Don't output example code and don't format it as markdown. " +
+               $"Only the actual json:\n\n{json}\n\n" +
+               $"Html starts now:\n\n{html}";
+
+var res = await Cli.Status("Thinking...", async () => await lm.GetCompletions(settings.SelectedModel, question));
 foreach(var m in res.Choices)
 {
     Console.WriteLine($"> {m.Message.Content}");
+}
+
+
+internal class TagsResponse
+{
+    [JsonPropertyName("tags")] public ImmutableArray<string> Tags { get; set; } = [];
+    [JsonPropertyName("reasoning")] public string Reasoning { get; set; } = "";
 }
